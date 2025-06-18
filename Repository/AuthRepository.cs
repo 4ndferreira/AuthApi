d88ce6a6b44d53e91aaf06ee39e74246bad1b5f8
Anderson Ferreira <anderson.ferreira@compasso.com.br>
2025-06-18 14:24:06 -0300

    Feature: criada a camada de repository para Auth

---
+++ Repository/AuthRepository.cs
@@ -0,0 +1,26 @@
+using AuthApi.Contracts.Auth;
+using AuthApi.Data;
+using AuthApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthApi.Repository;
+
+public class AuthRepository : IAuthRepository
+{
+  private readonly AppDbContext _context;
+
+  public AuthRepository(AppDbContext context)
+  {
+    _context = context ?? throw new ArgumentNullException(nameof(context));
+  }
+  public async Task CreateUserAsync(User user)
+  {
+    _context.Users.Add(user);
+    await _context.SaveChangesAsync();
+  }
+
+  public async Task<bool> UserExistsByEmailAsync(string email)
+  {
+    return await _context.Users.AnyAsync(u => u.Email == email);
+  }
+}
