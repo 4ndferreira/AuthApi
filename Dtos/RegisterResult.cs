c36a6a2983d947d894ccd7047b2e1fa7f463d2c5
Anderson Ferreira <anderson.ferreira@compasso.com.br>
2025-06-18 16:52:32 -0300

    Refactor: refatorado o tipo de Result

--- Dtos/RegisterResult.cs
+++
@@ -1,7 +0,0 @@
-namespace AuthApi.Dtos;
-
-public record RegisterResult(bool Sucess, string? Message = null, Guid? UserId = null)
-{
-  public static RegisterResult Success(Guid id) => new(true, null, id);
-  public static RegisterResult Failure(string message) => new(false, message);
-}
---
+++ Dtos/Result.cs
@@ -0,0 +1,10 @@
+namespace AuthApi.Dtos;
+
+public record Result<T>
+{
+  public bool Success { get; init; }
+  public string? Message { get; init; }
+  public T? Value { get; init; }
+  public static Result<T> SuccessResult(T value) => new() { Success = true, Value = value };
+  public static Result<T> Failure(string message) => new() { Message = message };
+}
