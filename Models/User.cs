f1c471391a3bbca5308ffebdef24959817523435
Anderson Ferreira <anderson.ferreira@compasso.com.br>
2025-06-18 16:50:40 -0300

    Feature: criado construtor para entidade User

--- Models/User.cs
+++ Models/User.cs
@@ -6,4 +6,10 @@ public class User
   public string? Email { get; set; } = null;
   public string? PasswordHash { get; set; } = null;
   public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+
+  public User(string email, string passwordHash)
+  {
+    Email = email;
+    PasswordHash = passwordHash;
+  }
 }
